6c5c1d5e068c8a137938d839a23b9735ad4751d1
Paula Gallucci Zurita <paula.gallucci@local.eurecat.org>
2022-04-30 12:04:16 +0200

    update models

--- database/get_top_parts.sql
+++ database/get_top_parts.sql
@@ -1,14 +1,18 @@
--- We recover the top 10 parts more used during reparations
+#standardSQL
 SELECT 
   model_id,
-  ARRAY_AGG( reference_id ORDER BY model_id, total_ref DESC LIMIT 10) data
+  v.name,
+  brand_id,
+  b.name
+  year,
+  ARRAY_AGG( struct(reference_id) ORDER BY model_id DESC LIMIT 10) references_used,
 
 FROM (
-    SELECT model_id, reference_id, count(reference_id) as total_ref
-  FROM `{REDACTED}.hackupc2022.sales`
+    SELECT model_id, reference_id, count(reference_id) as total_ref_used
+  FROM `bot-testing-345117.hackupc2022.sales`
   GROUP BY model_id, reference_id
-  ORDER BY model_id, total_ref desc
-)
-
-GROUP BY model_id
+  ORDER BY model_id, total_ref_used desc
+) s INNER JOIN
+`bot-testing-345117.hackupc2022.versions` v on s.model_id = v.id inner join `bot-testing-345117.hackupc2022.brands` b on b.id = v.brand_id
+GROUP BY model_id, brand_id, year, name
 ORDER BY model_id
--- main.py
+++ main.py
@@ -9,14 +9,15 @@ app = FastAPI()
 
 
 dummy_parts = [Parts(1, "wheel", 12),Parts(2, "manillar", 2),Parts(3, "sillin", 4)]
-dummy_models = [Model(1, " K 100 RS Motorsport   (1986-1988)", 1995, [Used_Part(dummy_parts[0], 13),Used_Part(dummy_parts[1], 1)])]
+dummy_models = [Model(1, " K 100 RS Motorsport   (1986-1988)", 1995, [Used_Part(dummy_parts[0], 13),Used_Part(dummy_parts[1], 1)], True),
+Model(1, "Moto X (1986-1988)", 1995, [Used_Part(dummy_parts[2], 1),Used_Part(dummy_parts[0], 1)], False)]
 
 
 ### DUMMY SECTION ###
 fake_items_db = [{"model": "R 100 RT   (1978-1996)"}, {"model": "K 75 S Special   (1986-1988)"}, {"model": "R 100 RS   (1987-1995)"}]
 
-@app.get("/warnings/")
-async def get_warnings():
+@app.get("/models/")
+async def get_models():
     return dummy_models
 
 @app.get("/infomodel/{model}")
